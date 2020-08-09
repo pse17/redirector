0ec71a7bcafdc1b32dc800ba75baed3441cdb998
pse17 <fis777@yandex.ru>
2020-08-09 16:53:39 +0500

    gitignore edit

--- tests.py
+++ tests.py
@@ -1,4 +1,5 @@
 import unittest
+from unittest.mock import patch
 import json
 import sys
 import os
@@ -10,18 +11,21 @@ sys.path.insert(0, os.path.dirname(__file__))
 
 class RedirectorTests(unittest.TestCase):
 
+    
     def setUp(self):
         SR.CONFIG_PATH = 'test.conf'
-        test_rule = {'yandex.ru': 'google.com'}
+        test_rule = {'lurkmore.to': 'wikipedia.org'}
         with open('test.conf', 'w') as test_conf:
             json.dump(test_rule, test_conf)
 
+    @patch('SR.read_input_line', return_value=' ')
     def test_read_config(self):
-        self.assertEqual(SR.read_config(), {'yandex.ru': 'google.com'})
+        self.assertEqual(SR.read_config(), {'lurkmore.to': 'wikipedia.org'})
 
+    @patch('SR.read_input_line', return_value=' ')
     def test_check_url(self):
-        test_url_check = 'zen.yandex.ru:443 127.0.0.1/localhost'
+        test_url_check = 'lurkmore.to 127.0.0.1/localhost'
         #test_url_uncheck = 'https://python.org'
 
-        self.assertEqual(SR.check_url(test_url_check), ("OK", '302:https://google.com'))
+        self.assertEqual(SR.check_url(test_url_check), ("OK", '302:https://wikipedia.org'))
         # self.assertEqual(SR.check_url(test_url_uncheck), ("ERR", "") )
